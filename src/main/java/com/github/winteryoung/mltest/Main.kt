655e3bb3b8f049d9e82fb1bc238cc025eb7bd5c9
Winter Young <513805252@qq.com>
2016-02-12 19:45:06 +0800

    update kotlin

--- src/main/java/com/github/winteryoung/mltest/Main.kt
+++ src/main/java/com/github/winteryoung/mltest/Main.kt
@@ -7,8 +7,8 @@ fun main(args: Array<String>) {
     val trainingData = readMnistTrainingData(mnistFolder)
     val testData = readMnistTestData(mnistFolder)
 
-    val nn = NeuralNetwork(layerSizes = listOf(784, 30, 10), learningRate = 3.0)
-    nn.train(trainingData.map { it.toLabeledData() }.toArrayList(), epochs = 300, miniBatchSize = 100) {
+    val nn = NeuralNetwork(layerSizes = listOf(784, 30, 10), learningRate = 1.0)
+    nn.train(trainingData.map { it.toLabeledData() }.toMutableList(), epochs = 300, miniBatchSize = 100) {
         val failures = ArrayList<Pair<LabeledDigitImage, Byte>>()
         for (labeledDigitImage in testData) {
             val prediction = nn.predict(labeledDigitImage.toLabeledData().data).maxIndex.toByte()
